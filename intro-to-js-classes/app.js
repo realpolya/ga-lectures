4b2a5ba3ebc2999ce4db365f10822ea7e4a96c9b
realpolya <ps2780@columbia.edu>
2024-09-09 15:22:47 -0400

    initiate js classes lecture

---
+++ intro-to-js-classes/app.js
@@ -0,0 +1,38 @@
+/* Intro to Javascript Classes */
+
+/*
+
+ In the style of object-oriented programming (OOP),
+classes offer a clear and efficient way to create reusable 
+parts of an application.
+
+Encapsulation:
+Bundling data and methods within an object.
+
+
+*/
+
+const myFirstCar = {
+    make: 'Toyota',
+    model: 'Corolla',
+    color: 'black',
+    isRunning: false,
+    start: function() {
+      myFirstCar.isRunning = true;
+      console.log('Running!');
+    },
+};
+
+// convention to capitalize Class (PascalCase)
+class Car {
+    // code to define class's properties and methods
+    // constructor method
+    constructor(carMake, carModel) {
+        this.make = carMake;
+        this.model = carModel;
+    }
+}
+
+const myCar = new Car();
+
+console.log(myCar);
