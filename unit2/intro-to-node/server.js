f4b50bbfd2d922db598f13a05350b5eee55dff77
realpolya <ps2780@columbia.edu>
2024-09-20 12:24:18 -0400

    work on validator

--- unit2/intro-to-node/server.js
+++ unit2/intro-to-node/server.js
@@ -6,3 +6,11 @@ const fs = require('fs');
 fs.writeFile('./hello.txt', 'hello, friend', () => {
     console.log("We did it!");
 })
+
+const validator = require('validator');
+
+console.log("email test: ", validator.isEmail('realpolya@gmail.com'));
+console.log("email test: ", validator.isEmail("love"));
+
+console.log("uppercase test: ", validator.isUppercase('hello'));
+console.log("uppercase test: ", validator.isUppercase('WONDER'));
