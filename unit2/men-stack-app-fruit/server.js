3cd3bf8b1a8f43746a7f244893e794d92fbb545d
realpolya <ps2780@columbia.edu>
2024-09-26 11:36:35 -0400

    import ejs, mongoose, dotenv, create template with ejs

--- unit2/men-stack-app-fruit/server.js
+++ unit2/men-stack-app-fruit/server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("Listening at 3000");
 })
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+})
+
