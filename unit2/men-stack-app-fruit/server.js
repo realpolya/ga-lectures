a6163fe971fa1a4304c3c2cec9ee5421a45f900f
realpolya <ps2780@columbia.edu>
2024-09-26 12:11:33 -0400

    update mongodb connection

--- unit2/men-stack-app-fruit/server.js
+++ unit2/men-stack-app-fruit/server.js
@@ -1,15 +1,24 @@
-import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
+// connect to mongoDB fruits collection
+mongoose.connect(process.env.MONGODB_URI);
+
+// check the connection
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+})
+
 app.listen(3000, () => {
     console.log("Listening at 3000");
-})
+});
 
 app.get("/", async (req, res) => {
     res.render("index.ejs");
-})
+});
+
 
