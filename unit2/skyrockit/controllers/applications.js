ec492705ef4a151d0f6276512762f65a321c1906
realpolya <ps2780@columbia.edu>
2024-10-01 12:29:37 -0400

    add navbar

--- unit2/skyrockit/controllers/applications.js
+++ unit2/skyrockit/controllers/applications.js
@@ -5,8 +5,9 @@ import User from '../models/user.js';
 
 router.get('/', (req, res) => {
     // res.send('Hello applications index route')
+    console.log(req.locals)
     try {
-        res.render('applications/index.ejs');
+        res.render('applications/index.ejs') // how to get the user object
     } catch (err) {
         console.error(err);
     }
