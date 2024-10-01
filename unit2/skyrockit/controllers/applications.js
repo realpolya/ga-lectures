46add1b51e427f6b7107e0c124608840d4729f64
realpolya <ps2780@columbia.edu>
2024-10-01 14:30:02 -0400

    add list view for applications

--- unit2/skyrockit/controllers/applications.js
+++ unit2/skyrockit/controllers/applications.js
@@ -3,11 +3,18 @@ const router = express.Router();
 
 import User from '../models/user.js';
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     // res.send('Hello applications index route')
     console.log(req.locals)
     try {
-        res.render('applications/index.ejs') // how to get the user object
+        //current user
+        const user = await User.findById(req.session.user._id);
+        // array of objects
+        const applications = user.applications; // or Array.from?
+        
+
+        res.render('applications/index.ejs', { applications }) // how to get the user object
+
     } catch (err) {
         console.error(err);
     }
@@ -16,5 +23,29 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
     res.render('applications/new.ejs');
 });
+
+router.post('/', async (req, res) => {
+    
+    try {
+        //look up the user
+        const user = await User.findById(req.session.user._id);
+
+        //push req.body to the applications array
+        user.applications.push(req.body);
+
+        //save changes to the database
+        await user.save();
+
+        //redirect to the applications view
+        res.redirect(`/users/${user._id}/applications`);
+
+    } catch (err) {
+        
+        console.log(err);
+        res.redirect('/');
+
+    }
+
+})
 
 export default router;
