84a592178fa47c82d8cc1b1dd405c87a2bfdb710
realpolya <ps2780@columbia.edu>
2024-10-14 10:27:19 -0400

    work on react state management

--- unit3/react-state-management/src/App.jsx
+++ unit3/react-state-management/src/App.jsx
@@ -1,8 +1,32 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  // const [count, setCount] = useState(0) 
+  // useState = hook is a special function
+  // [variable to hold data, function that manages that variable]
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  const [person, setPerson] = useState({
+    firstName: 'Polya',
+    lastName: 'Stepa',
+    hasPets: false,
+    age: 27
+  })
+
+  console.log('Dark mode is ', darkMode);
+  console.log('Person is ', person);
+
+
   return (
-    <h1>Hello world!</h1>
+    <>
+      <div className={darkMode ? 'dark' : 'light'}>
+        <h1>Hello world!</h1>
+        <p>Hello, my name is {person.firstName} {person.lastName}</p>
+        <p>I am {person.age} years old and I {person.hasPets ? 'have' : "don't have"} pets</p>
+      </div>
+    </>
   );
 }
 
