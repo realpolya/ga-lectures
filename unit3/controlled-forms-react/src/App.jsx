b454c4cec6a76638305b8295bd27a0b146b17e4b
realpolya <ps2780@columbia.edu>
2024-10-14 13:30:08 -0400

    work on controlled form lecture

--- unit3/controlled-forms-react/src/App.jsx
+++ unit3/controlled-forms-react/src/App.jsx
@@ -3,11 +3,26 @@ import './App.css'
 
 function App() {
 
+  const [cityInput, setCityInput] = useState('');
+
+  const handleChange = e => setCityInput(e.target.value);
+
   return (
     <>
-      <h1>Hello world!</h1>
+      <div>
+        <h1>Welcome!</h1>
+        <label htmlFor='cityInput'> City: </label>
+        <input 
+          id='cityInput' 
+          name='cityInput' 
+          type='text'
+          value={cityInput}
+          onChange={handleChange} 
+        />
+      </div>
     </>
   )
+
 }
 
 export default App
