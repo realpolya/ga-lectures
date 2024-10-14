6ab6e1327244127769201e3d3f8e080a50dc98b5
realpolya <ps2780@columbia.edu>
2024-10-14 14:08:02 -0400

    finish controlled forms in react

--- unit3/controlled-forms-react/src/App.jsx
+++ unit3/controlled-forms-react/src/App.jsx
@@ -3,24 +3,71 @@ import './App.css'
 
 function App() {
 
+  /* chapter 1 */
   const [cityInput, setCityInput] = useState('');
-
   const handleChange = e => setCityInput(e.target.value);
 
+  /* chapter 2 */
+  const [title, setTitle] = useState('Name shall appear');
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: ''
+  });
+
+  const handleFormData = e => setFormData({...formData, [e.target.name]: e.target.value });
+  const handleTitle = (formData) => setTitle(`Your name: ${formData.firstName} ${formData.lastName}`);
+  
+  const handleSubmit = e => {
+
+    // prevent navigation away from the page
+    e.preventDefault();
+    console.log("handling submit");
+
+    // update title
+    handleTitle(formData);
+
+    // clear formData
+    setFormData({firstName:'', lastName:''});
+
+  }
+ 
   return (
+
     <>
       <div>
-        <h1>Welcome!</h1>
-        <label htmlFor='cityInput'> City: </label>
-        <input 
-          id='cityInput' 
-          name='cityInput' 
-          type='text'
-          value={cityInput}
-          onChange={handleChange} 
-        />
+
+        <div className="first">
+          <h1>Chapter 1</h1>
+          <label htmlFor='cityInput'> City: </label>
+          <input
+            id='cityInput'
+            name='cityInput'
+            type='text'
+            value={cityInput}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="second">
+          <h1>Chapter 2</h1>
+          <h2>{title}</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor='firstName'>First Name: </label>
+            <input id='firstName' name='firstName' 
+              value={formData.firstName} onChange={handleFormData}/>
+
+            <label htmlFor='lastName'>Last Name: </label>
+            <input id='lastName' name='lastName' 
+              value={formData.lastName} onChange={handleFormData}/>
+
+            <button type='submit'>Submit your name</button>
+          </form>
+        </div>
+
       </div>
+
     </>
+
   )
 
 }
