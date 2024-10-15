0d494d1ddae90bc53ae11b7e5158f4c9fbfdd2e7
realpolya <ps2780@columbia.edu>
2024-10-15 14:23:01 -0400

    add fetch api request in fetching lecture

---
+++ unit3/fetching-data-react/src/services/weatherService.js
@@ -0,0 +1,17 @@
+const API_KEY = 'defa1f9185e54c1082f175902241510';
+const BASE_URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
+
+const show = async (city) => {
+    try {
+
+        const query = `&q=${city}`;
+        const res = await fetch(BASE_URL + query);
+        const data = await res.json();
+        console.log('Data ', data);
+
+    } catch (err) {
+
+        console.error(err);
+
+    }
+}
