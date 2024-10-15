e44543ddc170e2959f3742d1414a64f6c115a194
realpolya <ps2780@columbia.edu>
2024-10-15 16:03:37 -0400

    add fetchDefault

--- unit3/fetching-data-react/src/App.jsx
+++ unit3/fetching-data-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import * as weather from './services/weather.js'
 import SearchBar from './components/SearchBar.jsx';
 import WeatherDetails from './components/WeatherDetails.jsx';
@@ -8,6 +8,34 @@ function App() {
 
   /* states */
   const [weatherData, setWeatherData] = useState({});
+
+  /* effects */
+  useEffect(() => { 
+
+      let city = 'New York'
+
+      const fetchDefaultData = async (city) => {
+
+          // fetch data
+          const data = await weather.show(city);
+
+          // sort data
+          const sortedData = {
+            location: data.location.name,
+            temperature: data.current.temp_f,
+            condition: data.current.condition.text,
+          }
+
+          console.log(sortedData);
+
+          setWeatherData(sortedData);
+
+      }
+
+      // call the function
+      fetchDefaultData(city);
+
+  }, [])
 
   /* functions */
   const fetchData = async (city) => {
