96079691dedca3bd9509c67c453e8549ec5df814
realpolya <ps2780@columbia.edu>
2024-10-17 10:19:35 -0400

    finish css in js example

--- unit3/styling-in-react/src/App.jsx
+++ unit3/styling-in-react/src/App.jsx
@@ -2,6 +2,8 @@
 import './App.css';
 import Button1 from './components/normal-css/Button.jsx';
 import Button2 from './components/inline-css/Button.jsx';
+import Button3 from './components/css-in-js/Button.jsx';
+
 
 
 const App = () => {
@@ -10,6 +12,7 @@ const App = () => {
       <h1>Buttons</h1>
       <Button1 buttonText="Normal CSS" />
       <Button2 buttonText="Inline CSS" />
+      <Button3 buttonText="CSS in JS" />
     </div>
   );
 };
--- unit3/styling-in-react/src/components/css-in-js/Button.jsx
+++ unit3/styling-in-react/src/components/css-in-js/Button.jsx
@@ -1,6 +1,22 @@
+import { createUseStyles } from 'react-jss';
+
 function Button({ buttonText }) {
+  
+  const styles = createUseStyles({
+    btn: {
+      backgroundColor: '#24292e', // adding the styles in the object that we'll apply to our button
+      borderRadius: '8px',
+      color: '#f7df1c',
+      fontSize: '24pt',
+      padding: '15px 50px',
+      width: '300px',
+    },
+  });
+
+  const classes = styles();
+  
   return (
-    <button>{buttonText}</button>
+    <button className={classes.btn}>{buttonText}</button>
   )
 }
 
