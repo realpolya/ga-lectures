f1aa681f3f0da15ee61a7f16b80d48da5f8d4137
realpolya <ps2780@columbia.edu>
2024-11-06 12:09:15 -0500

    work on hash function

--- cslec/hash-tables/hash-tables-starter-code/hash-table.js
+++ cslec/hash-tables/hash-tables-starter-code/hash-table.js
@@ -3,47 +3,92 @@ class Node {
     this.data = [key, value];
     this.next = null;
   }
+
+  // no parenthesis for the key
   get key() {
-    // return the actual key from the data property
+    return this.data[0]
   }
   get value() {
-    // return the actual value from the data property
+    return this.data[1]
   }
 }
 
 // note: this is a simpler LinkedList class than in the Linked List lesson
 class LinkedList {
+
   constructor(){
-    // initialize a "head" property to null
+    this.head = null
   }
+
   add(key, value){
-    // create a new Node with the given data as its data property 
-    // if this list's head is null make that node the head, 
-    // otherwise add it to end of the list
+
+    let node = new Node(key, value)
+
+    if (!this.head) {
+
+      return this.head = node
+
+    } 
+
+    let current = this.head
+    while (current.next) {
+      current = current.next
+    }
+    current.next = node
+
+    return node
+
   }
   delete(key){
-    // search the list for a node whose data has a key that matches the key parameter
-    // remove it from the list and return it
-    // if no such node exists, return false
+
+    if (!this.head) return false;
+
+    let current = this.head
+
+    if (current.key === key) {
+        this.head = walker.next;
+        return walker
+    }
+
+    while (current.next) {
+
+      if (current.next.key === key) {
+        current.next = current.next.next
+        return current.next
+      }
+
+      current = current.next
+    }
+    
+    return false;
+
   }
+
   search(key){
-    // searches the list for a given key
-    // if it is found, return it
-    // if not, return false
+
+    if (!this.head) return false;
+
+    let current = this.head;
+
+    while (current) {
+      if (current.key === key) return current;
+      current = current.next;
+    }
+
+    return false;
+
   }  
 }
 
 class HashTable {
   constructor(size) {
-    // initialize table size - prime number size is recommended to avoid clustering
-    // initialize the table to have "size" number of elements, set to null
-    // the table will be an array named "table"
     this.table = new Array(size).fill(null);
   }
 
   hash(key) {
-    // calculate and return an integer value based key, like in the lesson
-    // remember, if you are using modulus, it is recommended to use a prime number to avoid clustering
+    let sum = 0
+    key.split('').forEach((char) => sum += char.charCodeAt())
+    return sum % this.table.length;
   }
 
   insert(key, value) {
