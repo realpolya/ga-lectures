da9df539629da5216991dba8f32b995776859dd6
realpolya <ps2780@columbia.edu>
2024-11-07 15:24:44 -0500

    finish level up for classes

--- unit4/py_classes/level-up.py
+++ unit4/py_classes/level-up.py
@@ -2,10 +2,11 @@ import random
 
 class BankAccount():
 
-    def __init__(self, owner, balance=0):
+    def __init__(self, owner, balance=0, overdraft=False):
         self.owner = owner
         self.balance = balance
         self.account = random.randint(11111, 99999)
+        self.overdraft = overdraft
 
     def display_balance(self):
         print(f"The account balance is now {self.balance}")
@@ -15,8 +16,11 @@ class BankAccount():
         self.display_balance()
     
     def withdraw(self, amount):
-        self.balance -= amount
-        self.display_balance()
+        if not self.overdraft and self.balance < amount:
+            return print("Transaction denied")
+        else:
+            self.balance -= amount
+            self.display_balance()
     
     def __str__(self):
         return f"Account {self.account} owned by {self.owner} has a balance of ${self.balance}"
@@ -28,8 +32,19 @@ print(account1)
 print(account2)
 
 account1.deposit(1600)
-account2.withdraw(500)
+account2.withdraw(1200)
 
 print(account1)
 print(account2)
 
+class SavingsAccount(BankAccount):
+
+    def __init__(self, owner, balance=0, overdraft=False):
+        BankAccount.__init__(self, owner, balance, overdraft)
+    
+    def withdraw(self, amount):
+        return print("No withdrawals permitted")
+
+account3 = SavingsAccount("Krissya", 790)
+print(account3)
+account3.withdraw(700)
