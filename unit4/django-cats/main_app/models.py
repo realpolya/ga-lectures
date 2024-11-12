84113629cda5e917adab94681841bda28f3b160b
realpolya <ps2780@columbia.edu>
2024-11-12 14:20:58 -0500

    add Feeding to django lab

--- unit4/django-cats/main_app/models.py
+++ unit4/django-cats/main_app/models.py
@@ -1,13 +1,45 @@
 from django.db import models
 
-# Create your models here.
+# Tuple of Tuples (1 - value, 2 - human readable version)
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+MOODS = (
+    ('M', 'Moody'),
+    ('H', 'Happy'),
+    ('E', 'Ecstatic'),
+    ('G', 'Grumpy'),
+    ('P', 'Peaceful')
+)
 
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    mood = models.CharField(
+        'Current Mood',
+        max_length=1,
+        choices=MOODS,
+        default=MOODS[1][0]
+    )
 
     def __str__(self):
-        return f"{self.name} is a happy cat {self.age} years old!"
+        return f"{self.get_mood_display()} cat {self.name}"
 
+
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE) # cat_id in db
+
+    def __str__(self):
+        # get_blank_display() for Field with choices
+        return f"{self.cat} consumed {self.get_meal_display()} on {self.date}"
--- unit4/django-cats/main_app/serializers.py
+++ unit4/django-cats/main_app/serializers.py
@@ -1,7 +1,14 @@
+# serializers are needed for Restful APIs
 from rest_framework import serializers
-from .models import Cat
+from .models import Cat, Feeding
 
 class CatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cat
         fields = '__all__'
+
+class FeedingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feeding
+        fields = '__all__'
+        read_only_fields = ('cat',)
