520c2e992c3ace2de9496903b9fbb7044c1462cc
realpolya <ps2780@columbia.edu>
2024-11-12 12:53:00 -0500

    work on django lecture

--- unit4/django-cats/main_app/urls.py
+++ unit4/django-cats/main_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import Home, CatList, CatDetail
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('cats/', CatList.as_view(), name='cat-list'),
+    path('cats/<int:id>', CatDetail.as_view(), name='cat-detail'),
 ]
--- unit4/django-cats/main_app/views.py
+++ unit4/django-cats/main_app/views.py
@@ -1,5 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from .models import Cat
+from .serializers import CatSerializer
 
 # Create your views here.
 class Home(APIView):
@@ -9,3 +12,12 @@ class Home(APIView):
             'message': 'Cats are here...'
         }
         return Response(content)
+
+class CatList(generics.ListCreateAPIView):
+    queryset = Cat.objects.all()
+    serializer_class  = CatSerializer
+
+class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cat.objects.all()
+    serializer_class  = CatSerializer
+    lookup_field = 'id'
