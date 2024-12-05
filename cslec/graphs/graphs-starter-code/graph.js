656c469c4251320de9dbb78285ab045b4997e6cf
realpolya <ps2780@columbia.edu>
2024-12-05 11:59:27 -0500

    done with simple graph

--- cslec/graphs/graphs-starter-code/graph.js
+++ cslec/graphs/graphs-starter-code/graph.js
@@ -1,18 +1,29 @@
 class Graph {
-  constructor() {
-    // Create a property called `nodes` and set it equal to an empty object.
-    // This will be our adjacency list.
-  }
 
-  addNode(node) {
-    // If the node value passed in does not already exist in our adjacency
-    // list, then add it as a key and set it equal to an empty array.
-  }
+    constructor() {
 
-  addEdge(node, edgeNode) {
-    // If the node exists in our adjacency list, then push the edge into the
-    // array of edges for that node.
-  }
+        // Create a property called `nodes` and set it equal to an empty object.
+        this.nodes = {}
+        // This will be our adjacency list.
+
+    }
+
+    addNode(node) {
+
+        // If the node value passed in does not already exist in our adjacency
+        // list, then add it as a key and set it equal to an empty array.
+        if (!this.nodes.keys?.includes(node)) this.nodes[node] = []
+
+    }
+
+    addEdge(node, edgeNode) {
+
+        // If the node exists in our adjacency list, then push the edge into the
+        // array of edges for that node.
+        if (!this.nodes.keys?.includes(node)) this.nodes[node].push(edgeNode)
+
+    }
+
 }
 
 module.exports = { Graph };
